df5f303f04a76a7f53441f9cff5826f3a7ab9b08
bahadir tasdelen <bahadirtasdelen@gmail.com>
2021-09-25 11:29:01 +0300

    Made changes in all scripts for runner registration

--- register.sh
+++ register.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i {{ ssh_key_location }}  $1@$(cat ./gitlab-runner | awk '{print$2}') 'gitlab-runner register --url "http://$(cat ./gitlab-server | awk '{print$2}')/" --registration-token "$2" --description "$HOSTNAME"  --executor "shell" --non-interactive'
+gitlab-runner register --url "http://35.234.103.122/" --registration-token Vm-myUj74nVdvs_6ZEFS --description "$HOSTNAME"  --executor "shell" --non-interactive
